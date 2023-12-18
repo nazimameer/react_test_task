bb2d421b58505849b3306c34193c3693b2e38430
NAZIM AMEER <nazimameerpp@gmail.com>
2023-12-18 20:51:31 +0530

    Navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Image from 'next/image'
-import { Logo, QMark } from '../assets'
+import React from "react";
+import Image from "next/image";
+import { Logo, QMark } from "../assets";
 export const Navbar = () => {
   return (
-    <nav className='flex justify-between p-5 pl-10 pr-20 w-full h-20 bg-white '> 
-      <Image src={Logo} alt='craxino' className='w-[15%] h-[40px]'/>
-      <Image src={QMark} alt='craxino'  className='w-[2.5%] h-[30px]'/>
-    </nav>
-  )
-}
-
- 
+    <header className="w-full absolute z-10 border-b-2 border-gray-200">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:pr-16 pr-6 pl-5 py-4">
+        <Image
+          src={Logo}
+          alt="craxino"
+          className="object-contain w-[50%] sm:w-[30%] h-[40px] md:h-[50px]"
+        />
+        <Image
+          src={QMark}
+          alt="craxino"
+          className="object-contain w-[9.5%] sm:w-[5%] h-[25px] md:h-[35px]"
+        />
+      </nav>
+    </header>
+  );
+};
