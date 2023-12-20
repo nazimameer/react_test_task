edae4b9471b41f752b94ff285e0544ba90f3b581
NAZIM AMEER <nazimameerpp@gmail.com>
2023-12-20 19:16:50 +0530

    refactored personalinfo

--- app/(root)/personalinfo/page.tsx
+++ app/(root)/personalinfo/page.tsx
@@ -1,27 +1,37 @@
-import { Tab } from "../../../components";
-import { PersonalForm } from "../../../components";
-const page = () => {
+import { Tab, PersonalForm } from "@/components";
+
+/**
+ * Personal Information Page Component
+ * This page displays personal information and provides a tab navigation.
+ */
+const PersonalInfoPage = () => {
   return (
     <div className="flex flex-col min-h-screen items-center pt-10">
+      {/* Tab Navigation */}
       <div className="pt-24 flex gap-3">
+        {/* Personal Info Tab (Active) */}
         <Tab hovered={true} number={1} route="/personalinfo" />
+
+        {/* Financial Info Tab (Inactive) */}
         <div>
           <Tab hovered={false} number={2} route="/financialinfo" />
         </div>
       </div>
+
+      {/* Page Title */}
       <div className="mt-5">
         <div
           color="gray"
           className="mt-1 font-bold text-xl flex w-full justify-center font-mono text-black"
-          // placeholder={undefined}
         >
           Personal information
         </div>
       </div>
 
+      {/* Personal Information Form */}
       <PersonalForm />
     </div>
   );
 };
 
-export default page;
+export default PersonalInfoPage;
