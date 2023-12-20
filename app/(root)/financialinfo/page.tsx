e1c97ca62f3e740db7c15e9c72d4aa6aadf8df43
NAZIM AMEER <nazimameerpp@gmail.com>
2023-12-20 19:14:59 +0530

    refactor financialInfo

--- app/(root)/financialinfo/page.tsx
+++ app/(root)/financialinfo/page.tsx
@@ -1,25 +1,35 @@
-import { Tab } from "../../../components";
-import { FinancialForm } from "../../../components";
-const page = () => {
+import { Tab, FinancialForm } from "@/components";
+
+/**
+ * Financial Information Page Component
+ * This page displays financial information and provides a tab navigation.
+ */
+const FinancialInfoPage = () => {
   return (
     <div className="flex flex-col min-h-screen items-center pt-10">
+      {/* Tab Navigation */}
       <div className="pt-24 flex gap-3">
+        {/* Personal Info Tab */}
         <Tab hovered={false} number={1} route="/personalinfo" />
+
+        {/* Financial Info Tab */}
         <Tab hovered={true} number={2} route="/financialinfo" />
       </div>
+
+      {/* Page Title */}
       <div className="mt-5">
         <div
           color="gray"
           className="mt-1 font-bold text-xl flex w-full justify-center font-mono text-black"
-          // placeholder={undefined}
         >
           Financial information
         </div>
       </div>
 
+      {/* Financial Information Form */}
       <FinancialForm />
     </div>
   );
 };
 
-export default page;
+export default FinancialInfoPage;
