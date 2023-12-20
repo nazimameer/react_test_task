ba2b9352ea23cbbd6d43f8adc282896e572a5b55
NAZIM AMEER <nazimameerpp@gmail.com>
2023-12-20 19:25:54 +0530

    refactor Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import { RegForm } from "../components";
+/**
+ * Home page component.
+ * Renders the main content for the home page, including the registration form.
+ * @returns React component representing the home page.
+ */
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -6,3 +11,4 @@ export default function Home() {
     </main>
   );
 }
+
