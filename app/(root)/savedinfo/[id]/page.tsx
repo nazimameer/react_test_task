217b3ca9c3f41543f3583ff1d54372960a1dbcbf
NAZIM AMEER <nazimameerpp@gmail.com>
2023-12-20 19:17:42 +0530

    refactored savedInfo

--- app/(root)/savedinfo/[id]/page.tsx
+++ app/(root)/savedinfo/[id]/page.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { SavedInfoForm } from "@/components";
+
+/**
+ * SavedInfo Component
+ * This page displays the saved information form.
+ */
 const SavedInfo = () => {
   return (
     <div className="flex flex-col min-h-screen items-center pt-24">
+      {/* Page Title */}
       <h1 className="text-xl font-bold">Your Saved Data</h1>
+
+      {/* Saved Information Form */}
       <SavedInfoForm />
     </div>
   );
