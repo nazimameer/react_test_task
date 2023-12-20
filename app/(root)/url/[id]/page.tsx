6830bf4370eb66ac2e2278c1e098880d25cdf18f
NAZIM AMEER <nazimameerpp@gmail.com>
2023-12-20 19:18:38 +0530

    refactored url dynamic root

--- app/(root)/url/[id]/page.tsx
+++ app/(root)/url/[id]/page.tsx
@@ -1,7 +1,13 @@
 import { CustomUrlInput } from "@/components";
+
+/**
+ * CustomUrl Component
+ * This page displays the CustomUrlInput component.
+ */
 const CustomUrl = () => {
   return (
     <div className="flex flex-col min-h-screen items-center pt-48">
+      {/* Custom URL Input Component */}
       <CustomUrlInput />
     </div>
   );
