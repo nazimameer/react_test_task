96262c84a67ec70d6f0a91007936d20c4df5d739
NAZIM AMEER <nazimameerpp@gmail.com>
2023-12-20 19:27:14 +0530

    refactored assets

--- assets/index.ts
+++ assets/index.ts
@@ -3,4 +3,14 @@ import QMark from "./Qmark.png";
 import Iicon from "./iicon.png";
 import HoveredIcon1 from "./hoveredIcon1.png";
 import UnhoveredIcon2 from "./unhoveredIcon2.png";
+
+/**
+ * Image module for importing various images.
+ * @module Images
+ * @exports Logo - The logo image.
+ * @exports QMark - Qmark image.
+ * @exports Iicon - Iicon image.
+ * @exports HoveredIcon1 - Hovered icon 1 image.
+ * @exports UnhoveredIcon2 - Unhovered icon 2 image.
+ */
 export { Logo, QMark, Iicon, HoveredIcon1, UnhoveredIcon2 };
